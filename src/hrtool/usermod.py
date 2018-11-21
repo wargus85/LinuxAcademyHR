29faebac19c03a2c238cf0dc5f39760e353c5bd0
Warren Argus <warren@warbel.net>
2018-11-21 16:55:52 +0800

    started on the adduser function

--- src/hrtool/usermod.py
+++ src/hrtool/usermod.py
@@ -1,10 +1,18 @@
 import grp, pwd, crypt, spwd, sys, subprocess, os
 
-def add_user(name):
+
+
+def add_user(name,groups,password):
     #
     # If adding a user returns an error (ie, the user exists)
     # Then try updating the user instead (call mod_user)
-    pass
+    print(f"Adding {name}")
+    try:
+        user = pwd.getpwnam(name)
+    except KeyError as err:
+        # User does not exist
+        adduser = subprocess.run(['useradd','-m','-s','/bin/bash','-p',password,'-G',groups[0], name],check=True)
+
     
 def del_user(name,delete=False):
     # deleting a user should be straight forward, it will give an error if the user doesn't exist, which we can
@@ -14,6 +22,7 @@ def del_user(name,delete=False):
         try:
             with open(os.devnull, 'w') as devnull:
                 deleteduser = subprocess.run(["userdel", "-r", name], check=True, stdout=devnull,stderr=devnull)
+                print(f"deleted user {name}")
 
         except subprocess.CalledProcessError as err:
             if err.returncode == 1:
--- tests/test_usermod.py
+++ tests/test_usermod.py
@@ -7,3 +7,4 @@ user = {
     'password':'$6$hPckApj16w9iJIoF$9aWAdWEeIsWcSAoMZL94Dw.aRXe/clyl0na8i2GhnfqKjTBfAnA/DH4RtEyp9lJTQbnIeZbJjWUC140.bBCCh.',
     'delete':'True'
     }
+
