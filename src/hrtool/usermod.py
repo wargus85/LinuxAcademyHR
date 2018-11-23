18a4c2f8bc744e77721eb1888cef3c92b978c41f
Warren Argus <warren@warbel.net>
2018-11-23 15:28:57 +0800

    started work on tests

--- src/hrtool/usermod.py
+++ src/hrtool/usermod.py
@@ -1,7 +1,5 @@
 import grp, pwd, crypt, spwd, sys, subprocess, os
 
-
-
 def add_user(name,groups,password):
     #
     # If adding a user returns an error (ie, the user exists)
@@ -14,27 +12,28 @@ def add_user(name,groups,password):
             #the group does not exist so add it in
             addgroup = subprocess.run(['adduser',group])
             print(f"Creating group: f{group}")
-    
-    print(f"Adding: {name}")
     try:
         user = pwd.getpwnam(name)
+        #If the above line works, then the user exists. So call an update:
+        add_user(name,groups,password)
     except KeyError as err:
         # User does not exist, so we can go ahead and add
+        print(f"Adding: {name}")
         with open(os.devnull, 'w') as devnull:
             adduser = subprocess.run(
                 ['useradd', '-m', '-s', '/bin/bash', '-p', password, '-G', groups[0], name], check=True)
-            print(f"Added user {name}")
+            print(f"Added: {name}")
 
     
 def del_user(name,delete=False):
     # deleting a user should be straight forward, it will give an error if the user doesn't exist, which we can
     # ignore and continue.
     if delete == True:
-        print(f"Removing user: {name}")
+        print(f"Removing: {name}")
         try:
             with open(os.devnull, 'w') as devnull:
                 deleteduser = subprocess.run(["userdel", "-r", name], check=True, stdout=devnull,stderr=devnull)
-                print(f"Deleted user {name}")
+                print(f"Removed: {name}")
 
         except subprocess.CalledProcessError as err:
             if err.returncode == 1:
@@ -48,7 +47,13 @@ def del_user(name,delete=False):
 def mod_user(name,groups,password):
     # Check that the user exists, if not, then create the user with a call
     # to add_user()
-
+    userexists= False
+    try:
+        exists = pwd.getpwnam(name)
+        userexists = True
+    except KeyError as err:
+        add_user(name,groups,password)
+        userexists = True
 
     # Check that all the groups exist:
     for group in groups[0].split(','):
@@ -56,10 +61,15 @@ def mod_user(name,groups,password):
             exists = grp.getgrnam(group)
         except KeyError as Err:
             #the group does not exist so add it in
-            addgroup = subprocess.run(['adduser',group])
+            addgroup = subprocess.run(['addgroup',group])
             print(f"Creating group: f{group}")
 
     #Modify the user now that all the groups exist.
-    #added more comment
-
+    print(f"modifying: {name}")
+    if userexists == True:
+        try:
+            moduser = subprocess.run(['usermod','-a','-G',groups[0],'-p',password,name])
+            print(f"modified user: {name}")
+        except:
+            print(f"something went wrong!")
     
--- tests/test_usermod.py
+++ tests/test_usermod.py
@@ -8,3 +8,42 @@ user = {
     'delete':'True'
     }
 
+
+
+def test_add_existing_user(mocker):
+    '''
+    Test to add a user that already exists will update the user instead
+    '''
+    mocker.patch('subprocess.run')
+    
+    pass
+
+def test_add_new_user():
+    '''
+    Test to add a user that does not already exist 
+    '''
+    pass
+
+def test_del_existing_user():
+    '''
+    test delete a user that exists
+    '''
+    pass
+
+def test_del_non_existing_user():
+    '''
+    test delete a user that does not exist
+    '''
+    pass
+
+def test_modify_existing_user():
+    '''
+    test modifying an existing user
+    '''
+    pass
+
+def test_modify_non_existing_user():
+    '''
+    test modifying an non existant user
+    '''
+    pass
